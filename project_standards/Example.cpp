802fe0e5e903022982f823678ca1fa8f6f52576b
Jenny Boucher <jebouche@c2r6p3.hive.fi>
2023-10-05 13:33:55 +0300

    add intial example files created by the script with additional examples added

---
+++ project_standards/Example.cpp
@@ -0,0 +1,38 @@
+
+#include "Example.hpp"
+
+/* CONSTRUCTORS */
+
+Example::Example( void ) 
+: initialize_(), all_(), member_(), variables_(), here_() {
+
+	/* default constructor */
+}
+
+Example::Example( /*add parameters*/ ) {
+
+	/* parameterized constructor */
+}
+
+Example::Example( const Example& to_copy ) {
+
+	/* copy constructor */
+} 
+
+/* DESTRUCTOR */
+
+Example::~Example( void ) {
+
+	/* destructor */
+} 
+
+/* OPERATOR OVERLOADS */
+
+Example&	Example::operator=( const Example& rhs ) {
+
+	/* copy assignment operator overload */
+}
+
+/* CLASS PUBLIC METHODS */
+
+/* CLASS PRIVATE METHODS */
