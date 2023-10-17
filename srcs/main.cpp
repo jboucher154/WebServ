8d2dc18848df259bce4bf11da160fc23ffad775b
Jenny Boucher <22949834+jboucher154@users.noreply.github.com>
2023-10-17 11:59:06 +0300

    Update main.cpp KAN-5

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -4,7 +4,7 @@
 int  main( int argc, char *argv[]) {
 
   if (argc == 2) {
-    std::cout << "parse will be called" << std::endl;
+    std::cout << "parse will be called!" << std::endl;
   }
   return (0);
 }
