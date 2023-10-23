c4b59ff380ca680d5054229d4456e8be154b4c9c
Jenny Boucher <jebouche@c2r5p3.hive.fi>
2023-10-23 13:38:45 +0300

    add intial files with mvp for 501, 404, and unspecified response codes. KAN-9

---
+++ include/ResponseCodes.hpp
@@ -0,0 +1,27 @@
+#ifndef RESPONSECODES_HPP
+# define RESPONSECODES_HPP
+
+# include <map>
+# include <string>
+
+class	ResponseCodes {
+
+	private:
+		ResponseCodes( void );
+		ResponseCodes( const ResponseCodes& to_copy );
+		~ResponseCodes( void );
+		ResponseCodes&	operator=( const ResponseCodes& to_copy );
+
+		/* PRIVATE METHODS AND MEMBERS */
+		static std::map<int, std::string>	codes_;
+
+		static void	intialize_codes( void );
+
+	public:
+		/* PUBLIC METHODS */
+		static std::string	getCodeElementBody( int code );
+		static std::string	getCodeStatusLine( int code );
+		static std::string	getCombinedStatusLineAndBody( int code );
+};
+
+#endif
--- srcs/ResponseCodes.cpp
+++ srcs/ResponseCodes.cpp
@@ -1,37 +1,67 @@
 
 #include "ResponseCodes.hpp"
 
+std::map<int, std::string> ResponseCodes::codes_;
+
 /* CONSTRUCTORS */
 
-ResponseCodes::ResponseCodes( void ) {
-
-	/* default constructor */
-}
-
-ResponseCodes::ResponseCodes( /*add parameters*/ ) {
-
-	/* parameterized constructor */
-}
+ResponseCodes::ResponseCodes( void ) { /* default constructor */ }
 
 ResponseCodes::ResponseCodes( const ResponseCodes& to_copy ) {
 
-	/* copy constructor */
+	*this = to_copy;
 } 
 
 /* DESTRUCTOR */
 
-ResponseCodes::~ResponseCodes( void ) {
-
-	/* destructor */
-} 
+ResponseCodes::~ResponseCodes( void ) {	/* destructor */ } 
 
 /* OPERATOR OVERLOADS */
 
 ResponseCodes&	ResponseCodes::operator=( const ResponseCodes& rhs ) {
 
-	/* copy assignment operator overload */
+	if (this != &rhs ) {
+		return (*this);
+	}
+	return (*this);
 }
 
 /* CLASS PUBLIC METHODS */
 
+std::string	ResponseCodes::getCodeStatusLine( int code ) {
+
+	if (ResponseCodes::codes_.empty())
+		ResponseCodes::intialize_codes();
+	if (ResponseCodes::codes_[code].empty())
+		return ("HTTP/1.1 500 Internal Server Error\r\n");
+	std::string element = "HTTP/1.1 " + std::to_string(code) + " " + ResponseCodes::codes_[code] + "\r\n";
+	return (element);
+}
+
+std::string	ResponseCodes::getCodeElementBody( int code ) {
+
+	if (ResponseCodes::codes_.empty())
+		ResponseCodes::intialize_codes();
+	std::string message = ResponseCodes::codes_[code];
+	if (message.empty())
+		message = "Unknown Error";
+	std::string element = "\r\n<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<title>" + std::to_string(code) + " ";
+	element += message + "</title>\n</head>\n<body>\n<h1>" + std::to_string(code) + " " + message + "</h1>\n</body>\n</html>\r\n";
+	return (element);
+}
+
+std::string	ResponseCodes::getCombinedStatusLineAndBody( int code ) {
+
+	if (ResponseCodes::codes_.empty())
+		ResponseCodes::intialize_codes();
+	std::string element = ResponseCodes::getCodeStatusLine(code) + ResponseCodes::getCodeElementBody(code);
+	return (element);
+}
+
 /* CLASS PRIVATE METHODS */
+
+void	ResponseCodes::intialize_codes( void ) {
+
+	ResponseCodes::codes_[501] = "Not Implemented";
+	ResponseCodes::codes_[404] = "Not Found";
+}
