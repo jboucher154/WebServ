9c339b3a303c80a97ab7c0e9ac3477f9a6d953d9
Jenny Boucher <jebouche@c2r5p3.hive.fi>
2023-10-23 13:35:04 +0300

    add ignore rule for local test directory

---
+++ srcs/ResponseCodes.cpp
@@ -0,0 +1,37 @@
+
+#include "ResponseCodes.hpp"
+
+/* CONSTRUCTORS */
+
+ResponseCodes::ResponseCodes( void ) {
+
+	/* default constructor */
+}
+
+ResponseCodes::ResponseCodes( /*add parameters*/ ) {
+
+	/* parameterized constructor */
+}
+
+ResponseCodes::ResponseCodes( const ResponseCodes& to_copy ) {
+
+	/* copy constructor */
+} 
+
+/* DESTRUCTOR */
+
+ResponseCodes::~ResponseCodes( void ) {
+
+	/* destructor */
+} 
+
+/* OPERATOR OVERLOADS */
+
+ResponseCodes&	ResponseCodes::operator=( const ResponseCodes& rhs ) {
+
+	/* copy assignment operator overload */
+}
+
+/* CLASS PUBLIC METHODS */
+
+/* CLASS PRIVATE METHODS */
