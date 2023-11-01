b84f1da82a411dec65ae0466fe3f4f662a613ced
Sakari Salmi <sakarisalmi@hotmail.com>
2023-11-01 15:57:04 +0200

    KAN-25 removed sockets.hpp, added space to all includes and defines in hpp files

--- include/Client.hpp
+++ include/Client.hpp
@@ -1,9 +1,9 @@
 #ifndef CLIENT_HPP
-#define	CLIENT_HPP
+# define	CLIENT_HPP
 
-#include "Request.hpp"
-#include "Response.hpp"
-#include "Server.hpp"
+# include "Request.hpp"
+# include "Response.hpp"
+# include "Server.hpp"
 
 /*! \brief Brief description.
 *         Brief description continued.
