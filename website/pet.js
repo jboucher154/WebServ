8c79b359b1b3dd73032976bb58a5ecdb016d0fa7
azarSarikhani <azar.sarikhani.agh@gmail.com>
2023-12-13 13:51:32 +0200

    KAN-54 add comment box next to comment buttoon

--- website/pet.js
+++ website/pet.js
@@ -1,3 +1,11 @@
 function addComments() {
 	
   }
+
+// Assuming you have a function to detect when the user starts typing
+function onCommentTyping() {
+  document.querySelector('.comment_box').classList.remove('comment_text');
+}
+
+// Add an event listener to the textarea or input where the user writes a comment
+document.getElementById('commentInput').addEventListener('input', onCommentTyping);
