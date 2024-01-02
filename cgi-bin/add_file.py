4423a2cc09cb610e78d5e1989b692522ea1a9738
azarSarikhani <azar.sarikhani.agh@gmail.com>
2024-01-02 11:20:20 +0200

    KAN-59 change form.html so that photo will be sent alongside form data, add i add file python script

--- cgi-bin/add_file.py
+++ cgi-bin/add_file.py
@@ -1,34 +1,58 @@
-import argparse
+# import argparse
+# import sys
+# import os
+
+# def main():
+#     # parser = argparse.ArgumentParser(description='A simple Python script with arguments.')
+#     # parser.add_argument('input_strings', nargs='*', help='Array of input strings')
+#     # parser.add_argument('--output', '-o', help='Path to the output file (optional)')
+#     # parser.add_argument('--verbose', '-v', action='store_true', help='Enable verbose mode')
+
+#     # args = parser.parse_args()
+
+#     # Check if stdin is provided
+#     # if not sys.stdin.isatty():
+#     #     # Read input strings from stdin
+#     #     input_strings = [s.strip() for s in sys.stdin.readlines()]
+#     # else:
+#     #     input_strings = args.input_strings
+
+#     # print('Command-line arguments:', args.input_strings)
+#     # print('Input strings from stdin:', input_strings)
+#     path_translated = os.environ.get('PATH_TRANSLATED')
+#     html_content =  "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<ul>\n\t\t\t<li>" + path_translated + "</li>"
+#     query_string = os.environ.get("QUERY_STRING")
+#     html_content += "<li>" + query_string + "</li>" + "\n\t\t</ul>\n\t</body>\n</html>"
+#     # if args.output:
+#     #     print('Output file:', args.output)
+
+#     # if args.verbose:
+#     #     print('Verbose mode enabled')
+#     #     # Additional verbose information
+#     #     print('Performing some extra operations...')
+#     sys.stdout.write(html_content)
+
+
 import sys
-import os
 
-def other():
-    # parser = argparse.ArgumentParser(description='A simple Python script with arguments.')
-    # parser.add_argument('input_strings', nargs='*', help='Array of input strings')
-    # parser.add_argument('--output', '-o', help='Path to the output file (optional)')
-    # parser.add_argument('--verbose', '-v', action='store_true', help='Enable verbose mode')
+def save_binary_to_file(file_path, output_file_path):
+    try:
+        with open(file_path, 'rb') as input_file:
+            # Read binary data from the input file
+            binary_data = input_file.read()
 
-    # args = parser.parse_args()
+            # Write binary data to the output file
+            with open(output_file_path, 'wb') as output_file:
+                output_file.write(binary_data)
 
-    # Check if stdin is provided
-    # if not sys.stdin.isatty():
-    #     # Read input strings from stdin
-    #     input_strings = [s.strip() for s in sys.stdin.readlines()]
-    # else:
-    #     input_strings = args.input_strings
+        # print(f'Successfully saved binary data to {output_file_path}')
+    except Exception as e:
+        print(f'Error saving binary data: {e}')
 
-    # print('Command-line arguments:', args.input_strings)
-    # print('Input strings from stdin:', input_strings)
-    path_translated = os.environ.get('PATH_TRANSLATED')
-    html_content =  "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<ul>\n\t\t\t<li>" + path_translated + "</li>"
-    query_string = os.environ.get("QUERY_STRING")
-    html_content += "<li>" + query_string + "</li>" + "\n\t\t</ul>\n\t</body>\n</html>"
-    # if args.output:
-    #     print('Output file:', args.output)
+# # Example usage
+# input_file_path = 'input.bin'
+# output_file_path = 'output.bin'
+# save_binary_to_file(input_file_path, output_file_path)
 
-    # if args.verbose:
-    #     print('Verbose mode enabled')
-    #     # Additional verbose information
-    #     print('Performing some extra operations...')
-    sys.stdout.write(html_content)
-    
+if __name__ == '__main__':
+   save_binary_to_file()
