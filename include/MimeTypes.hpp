d4da2a14fb3f51402d86c115d8ddbdbe936e0665
sakari salmi <sakarisalmi@sakaris-MacBook-Pro.local>
2024-01-09 13:32:39 +0200

    KAN-107 created doxygen document for MimeTypes class, Jenny had already done the documents for the functions

--- include/MimeTypes.hpp
+++ include/MimeTypes.hpp
@@ -4,6 +4,13 @@
 # include <map>
 # include <string>
 
+/*! \brief MimeTypes is a utility class for handling MIME types within the web server.
+ *
+ *	@class This class provides functionality to map file extensions to their corresponding
+ *	MIME types. It is designed to be used within the web server to accurately identify
+ *	the content type of files based on their extensions. The class is implemented
+ *	as a singleton to ensure a single centralized instance managing MIME types.
+ */
 class	MimeTypes {
 
 	private:
