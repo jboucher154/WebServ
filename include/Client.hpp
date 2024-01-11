f032de7817d95dd1b071a6cc9acd36a90e49cf14
jen <jboucher154@gmail.com>
2024-01-11 06:07:22 +0200

    add more documetnation and sketch out potential refactors

--- include/Client.hpp
+++ include/Client.hpp
@@ -6,27 +6,33 @@
 # include "Server.hpp"
 # include "CgiHandler.hpp"
 
+# include <cstring>
 
+/*! \brief forward declaration for cgiHandlerclass */
 class CgiHandler;
 
-/*! \brief Brief description.
-*         Brief description continued.
+/*! \brief class contains all relevant information for server client
 *
-*  Detailed description starts here.
+*	@class Client
+*	Client class contains the clients file descritpor, address info, the server
+*	it connected with, along with the objects to handle the requests and responses
+*	for the client.
+*	
 */
 class	Client {
 	private:
-		int					fd_;					// client's file (socket) descriptor
-		struct sockaddr_in	address_;				// client's address					
-		time_t				latest_time_;			// time of client's latest action (used for disconneting and server automatic shutdown)
-		Server*				server_;				// pointer to the server the client is connected to
-		int					server_fd_;				// file (socket) descriptor of the server the client is connected to
 
-		Request				request_;
-		Response			response_;
-		CgiHandler*			cgi_handler_;
+		int					fd_; /*!< \brief client's file (socket) descriptor */
+		struct sockaddr_in	address_; /*!< \brief client's address information */			
+		time_t				latest_time_; /*!< \brief time of client's latest action (used for disconneting and server automatic shutdown) */ 
+		Server*				server_; /*!< \brief pointer to the server the client is connected to */
+		int					server_fd_; /*!< \brief file (socket) descriptor of the server the client is connected to */ 
+		Request				request_; /*!< \brief requst object to parse and handle client requests */
+		Response			response_; /*!< \brief response object to generate HTTP responses */
+		CgiHandler*			cgi_handler_; /*!< \brief object to run cgi processes */
 
 	public:
+	
 		Client( void );
 		Client( int server_fd, Server* server );
 		Client( const Client& other );
@@ -37,11 +43,11 @@ class	Client {
 		bool					startCgiResponse( void );
 		void					finishCgiResponse( void );
 
-		// setters
+		/* SETTERS */
 		void					setLatestTimeForClientAndServer( void );
 		void					setServerAndFd( Server* server, int new_server_fd );
 
-		// getters
+		/* GETTERS */
 		int						getFd( void ) const;
 		time_t					getLatestTime( void ) const;
 		Server*					getServer( void ) const;
@@ -49,14 +55,12 @@ class	Client {
 		std::string				getClientHost( void ) const;
 		struct sockaddr_in&		getAddress( void );
 		CgiHandler*				getCgiHandler( void );
-
 		Request&				getRequest( void );
 		Response&				getResponse( void );
 		const std::string&		getResponseString( void );
 		void					addToRequest( char* message, size_t bytes_read );
 		void					resetResponse( void );
 		void					resetRequest( void );
-
 };
 
 #endif
